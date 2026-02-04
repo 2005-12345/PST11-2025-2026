2d345ceb419ac13ce60905f650559fb26c38e24b
Devadharshini V <devadharshini2024@gmail.com>
2026-02-04 19:57:38 +0530

    Add files via upload

---
+++ oops concept/OOPS2.java
@@ -0,0 +1,72 @@
+package PST;
+
+
+
+class CSEA{
+
+	public static void method1(){
+		
+		System.out.println("Madhu");
+		method1a();
+		method1b();
+		method1c();
+		 
+	}
+	public static void method1a(){
+		
+		System.out.println("Deva");
+		}
+	public static void method1b(){
+		System.out.println("Dharshini");
+		 
+	}
+	public static void method1c(){
+		System.out.println("Kaviya");
+		 
+	}
+	}
+public class OOPS2{
+	public static void main(String[] args){
+		CSEA obj=new CSEA();
+		obj.method1();
+		
+		CSEB obj1=new CSEB();
+		obj1.method2();
+		
+		
+		 
+	}
+
+
+	
+
+	
+	}
+	
+	class CSEB{
+		public static void method2(){
+			
+			System.out.println("Ahisha");
+			method2a();
+			method2b();
+			method2c();
+			 
+		}
+		public static void method2a(){
+			
+			System.out.println("Kiruthi");
+			}
+		public static void method2b(){
+			System.out.println("Varsha");
+			 
+		}
+		public static void method2c(){
+			System.out.println("Sakthi");
+			 
+		}
+		
+		
+		
+		
+		
+}
